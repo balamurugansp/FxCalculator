dd3d3a2cecbcd873612692a292ae64fc8048e8fd
balamurugansp <spbalamurugan@gmail.com>
2017-07-10 22:57:56 +0530

    Create CurrencyValue.java

---
+++ src/main/java/com/anz/fxcalc/constants/CurrencyValue.java
@@ -0,0 +1,21 @@
+package com.anz.fxcalc.constants;
+
+/**
+ * @author balamurugan
+ *
+ */
+public final class CurrencyValue {
+		CurrencyValue(){}
+	
+		public static final double AUD_USD = 0.8371;
+	    public static final double CAD_USD = 0.8711;
+	    public static final double USD_CNY = 6.1715;
+	    public static final double EUR_USD = 1.2315;
+	    public static final double GBP_USD = 1.5683;
+	    public static final double NZD_USD = 0.7750;
+	    public static final double USD_JPY = 119.95;
+	    public static final double EUR_CZK = 27.6028;
+	    public static final double EUR_DKK = 7.4405;
+	    public static final double EUR_NOK = 8.6651;
+ 
+}
